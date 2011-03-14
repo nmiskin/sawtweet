f8e9ad0ca10148e23324b5114afd8c850d5d048d
Nate Miskin <nmiskin@techmedianetwork.com>
2011-03-14 10:16:42 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sawtweet::Application.config.secret_token = 'a1a18678a84e6c3522dc43abafa7f9743607801b331902e5ee62c0f24b83ef7b4457637a7b5106bd7793663716575ae12662b98e7179377d92ac39ef9c550bc3'
